1f4d688c28feda50c7ff753c7a6a1f8d44e7f399
ApoKads <112252748+ApoKads@users.noreply.github.com>
2024-04-03 19:39:36 +0700

    Add files via upload

---
+++ Page 1/halaman1.js
@@ -0,0 +1,42 @@
+const tabMenu = document.querySelector(".trending-atas")
+
+let activeDrag = false;
+
+tabMenu.addEventListener("mousemove",(drag)=>{
+    if(!activeDrag)return;
+    tabMenu.scrollLeft -=drag.movementX;
+});
+
+tabMenu.addEventListener("mousedown",()=>{
+    activeDrag = true;
+});
+
+document.addEventListener("mouseup", ()=>{
+    activeDrag=false;
+})
+
+
+let subMenu = document.getElementById("dropdown-browse");
+let browse = document.getElementById("Browse");
+function toggleMenu()
+{
+    subMenu.classList.toggle("open");
+    browse.classList.toggle("open");
+    document.addEventListener("mousedown", (event) => {
+        if (!subMenu.contains(event.target) && !browse.contains(event.target)) {
+            // Clicked outside the menu, remove "open" class
+            subMenu.classList.remove("open");
+            browse.classList.remove("open");
+        }
+    });
+}
+
+let best = document.getElementById("best");
+let viewDropDown = document.getElementById("best-luar-dropdown");
+let viewBest = document.getElementById("best-luar");
+    function toggleDropBest()
+    {
+        best.classList.toggle("open");
+        viewDropDown.classList.toggle("open");
+        viewBest.classList.toggle("open");
+    }
